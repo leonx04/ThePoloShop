abff587acdf105606312f6f1cb3208f31543cfdc
leonx04 <jungnguyen2108@gmail.com>
2024-06-09 12:16:22 +0700

    Create Project Spring boot with IDE

---
+++ WebsiteBe/src/main/java/com/example/websitebe/WebsiteBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.websitebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteBeApplication.class, args);
+    }
+
+}
---
+++ WebsiteBe/src/test/java/com/example/websitebe/WebsiteBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websitebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
